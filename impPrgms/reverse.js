c5b58482c5a82f5f0753480c03662ec08dc41726
avinash0903 <avinash.skzr@gmail.com>
2021-08-20 21:33:25 +0530

    2 files modified

--- impPrgms/reverse.js
+++ impPrgms/reverse.js
@@ -9,14 +9,28 @@ function reverse(str) {
 }
 let reversed = reverse(456);
 console.log(reversed);
-
-
-function reverse(str){
+//2
+function reverse(str) {
   var newString = '';
-  for(let i = str.length - 1; i>=0; i--){
+  for (let i = str.length - 1; i >= 0; i--) {
     newString = newString + str[i];
   }
   return newString;
 }
-let rev = reverse('avinash')
+let rev = reverse('avinash');
 console.log(rev);
+
+//palindrome
+function palindrome(str) {
+  const re = /[\Z_]/g;
+  str = str.toLowerCase().replace(re, '');
+  const len = str.length;
+  for (let i = 0; i < len; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+const b = palindrome('abba');
+console.log(b);
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function reverse(str) {
-  var newString = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    newString = newString + str[i];
+function palindrome(str) {
+  const re = /[\Z_]/g;
+  str = str.toLowerCase().replace(re, '');
+  const len = str.length;
+  for (let i = 0; i < len; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
   }
-  return newString;
+  return true;
 }
-let rev = reverse('avinash');
-console.log(rev);
+const b = palindrome('abba');
+console.log(b);
